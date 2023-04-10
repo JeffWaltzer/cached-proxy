4668c37242966ec97c4765d57ebc2bf2e751d4bd
Jeff Waltzer <jeffwaltzer@gmail.com>
2023-04-09 22:11:57 -0400

    more refactorings

--- lib/cacheme.rb
+++ lib/cacheme.rb
@@ -25,21 +25,42 @@ def origin_response(request)
   origin_url = "#{request['uri']}?#{request['querystring']}"
   origin_uri = URI(origin_url)
   origin_request = Net::HTTP::Get.new(origin_uri)
-  request['headers'].each { |name, value| origin_request[name] = value[0]['value'] }
-  response = Net::HTTP.start(origin_uri.hostname, origin_uri.port, use_ssl: true,
-                             verify_mode: OpenSSL::SSL::VERIFY_PEER) do |http|
+
+  request['headers'].each do |name, value|
+    origin_request[name] = value[0]['value']
+  end
+
+  response = Net::HTTP.start(
+    origin_uri.hostname,
+    origin_uri.port,
+    use_ssl: true,
+    verify_mode: OpenSSL::SSL::VERIFY_PEER
+  ) do |http|
     http.request(origin_request)
   end
-  cache_key = generate_cache_key(request['uri'], request['querystring'])
-  cache_response(cache_key, response.body) if response.code == '200'
+
+  if response.code == '200'
+    cache_key = generate_cache_key(request['uri'], request['querystring'])
+    cache_response(cache_key, response.body)
+  end
+
   response
 end
-q
-def build_response(status_code, body, cached:)
+
+def build_response(status_code,
+                   body,
+                   cached:)
   {
     status: status_code.to_s,
     statusDescription: 'OK',
-    headers: { 'cache-hit': [{ key: 'Cache-Hit', value: cached.to_s }] },
+    headers: {
+      'cache-hit': [
+        {
+          key: 'Cache-Hit',
+          value: cached.to_s
+        }
+      ]
+    },
     body:
   }
 end
